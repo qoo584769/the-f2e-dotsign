598a64794cf49b0502af73e549712bf7eea962b8
ming wu <a85062@pchome.com.tw>
2024-05-10 01:31:08 +0800

    修改設定檔

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/the-f2e-dotsign/',
+  base: '/the-f2e-dotsign/dist/',
   plugins: [
     react(),
     topLevelAwait({
